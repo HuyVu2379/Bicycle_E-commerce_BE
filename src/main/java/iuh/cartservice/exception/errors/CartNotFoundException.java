22133ca695a5a8d56cb675bf02d6181b82bd1af9
HuyVu2379 <21021491.huy@student.iuh.edu.vn>
2025-03-07 23:54:50 +0700

    RESLOVE CONFLICT

--- src/main/java/iuh/cartservice/exception/errors/CartNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package iuh.cartservice.exception.errors;
-
-public class CartNotFoundException extends RuntimeException {
-    public CartNotFoundException(String message) {
-        super(message);
-    }
-}
