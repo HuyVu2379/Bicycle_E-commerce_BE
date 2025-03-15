63970a4cb3566f36e5a7d672f5c64c38041e9bb1
PhamLeThanhNhiet <nhiethiz@gmail.com>
2025-03-15 17:02:46 +0700

    done supplier service

--- ProductService/src/main/java/iuh/productservice/services/Impl/SupplierServiceImpl.java
+++ ProductService/src/main/java/iuh/productservice/services/Impl/SupplierServiceImpl.java
@@ -1,4 +1,47 @@
 package iuh.productservice.services.Impl;
 
-public class SupplierServiceImpl {
+import iuh.productservice.entities.Supplier;
+import iuh.productservice.repositories.SupplierRepository;
+import iuh.productservice.services.SupplierService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class SupplierServiceImpl implements SupplierService {
+    @Autowired
+    private SupplierRepository supplierRepository;
+
+    @Override
+    public Optional<Supplier> createSupplier(Supplier supplier) {
+        return Optional.of(supplierRepository.save(supplier));
+    }
+
+    @Override
+    public Optional<Supplier> updateSupplier(Supplier supplier) {
+        if(supplierRepository.findById(supplier.getSupplierId()).isEmpty()){
+            return Optional.empty();
+        }
+        return Optional.of(supplierRepository.save(supplier));
+    }
+
+    @Override
+    public Optional<Supplier> getSupplierById(String supplierId) {
+        return supplierRepository.findById(supplierId);
+    }
+
+    @Override
+    public Optional<Supplier> getSupplierByEmail(String email) {
+        return supplierRepository.getSupplierByEmail(email);
+    }
+
+    @Override
+    public boolean deleteSupplier(String supplierId) {
+        if(supplierRepository.findById(supplierId).isEmpty()){
+            return false;
+        }
+        supplierRepository.deleteById(supplierId);
+        return true;
+    }
 }
--- ProductService/src/main/java/iuh/productservice/services/SupplierService.java
+++ ProductService/src/main/java/iuh/productservice/services/SupplierService.java
@@ -1,7 +1,15 @@
 package iuh.productservice.services;
 
+import iuh.productservice.entities.Supplier;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public interface SupplierService {
+    Optional<Supplier> createSupplier(Supplier supplier);
+    Optional<Supplier> updateSupplier(Supplier supplier);
+    Optional<Supplier> getSupplierById(String supplierId);
+    Optional<Supplier> getSupplierByEmail(String email);
+    boolean deleteSupplier(String supplierId);
 }
