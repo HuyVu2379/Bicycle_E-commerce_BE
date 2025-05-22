6a1819c7d547d1da99358ce7150de2c25b3a810e
HuyVu2379 <21021491.huy@student.iuh.edu.vn>
2025-05-22 11:02:58 +0700

    chore: update payment

--- PaymentService/src/main/java/iuh/paymentservice/services/Impl/PaymentServiceImpl.java
+++ PaymentService/src/main/java/iuh/paymentservice/services/Impl/PaymentServiceImpl.java
@@ -129,14 +129,15 @@ public class PaymentServiceImpl implements PaymentService {
         vnp_Params.put("vnp_IpnUrl", vnPayConfig.getIpnUrl());
         vnp_Params.put("vnp_IpAddr", vnp_IpAddr);
 
-        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
+        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
         SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
         String vnp_CreateDate = formatter.format(cld.getTime());
         vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
-
-        cld.add(Calendar.MINUTE, 15);
+        logger.info("vnp_CreateDate: {}", vnp_CreateDate);
+        cld.add(Calendar.MINUTE, 30); // Tăng lên 30 phút
         String vnp_ExpireDate = formatter.format(cld.getTime());
         vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
+        logger.info("vnp_ExpireDate: {}", vnp_ExpireDate);
 
         StringBuilder hashData = new StringBuilder();
         StringBuilder query = new StringBuilder();
